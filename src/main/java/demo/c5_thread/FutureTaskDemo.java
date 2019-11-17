6ac0a07649e03e93f90f4df26cba86cb54639635
MY <441015782@qq.com>
2019-11-17 23:22:42 +0800

    commit

--- src/main/java/demo/c5_thread/FutureTaskDemo.java
+++ src/main/java/demo/c5_thread/FutureTaskDemo.java
@@ -8,6 +8,7 @@ import java.util.concurrent.*;
 @Slf4j
 public class FutureTaskDemo {
 
+    //拿到子线程的返回值
     @Test
     public void initCallable() throws ExecutionException, InterruptedException {
         FutureTask futureTask = new FutureTask(new Callable<String>() {
@@ -23,6 +24,7 @@ public class FutureTaskDemo {
         log.info("拿到值了，为 {}", result);
     }
 
+    //Runnable只能写死返回值
     @Test
     public void initRunnable() throws ExecutionException, InterruptedException {
         FutureTask<String> futureTask = new FutureTask<String>(new Runnable() {
@@ -30,28 +32,35 @@ public class FutureTaskDemo {
             public void run() {
                 log.info("{} is run", Thread.currentThread().getName());
             }
-        }, null);
+        }, "finish");
         futureTask.run();
         String result = futureTask.get();
         log.info("run end,result is {}", result);
     }
 
+    // 使用线程池运行FutureTask
     @Test
     public void testGet() throws ExecutionException, InterruptedException {
         ThreadPoolExecutor executor = new ThreadPoolExecutor(3, 3, 0L, TimeUnit.MILLISECONDS,
                 new LinkedBlockingQueue<>());
-        FutureTask futureTask = new FutureTask(new Callable<String>() {
-            @Override
-            public String call() throws Exception {
-                Thread.sleep(3000);
-                return "我是子线程" + Thread.currentThread().getName();
-            }
-        });
+//        FutureTask 我们叫做任务，入参是 Callable，是对 Callable 的包装，方便线程池的使用；
+        FutureTask futureTask = new FutureTask(
+//                Callable 定义我们需要做的事情，是可以有返回值的；
+                new Callable<String>() {
+                    @Override
+                    public String call() throws Exception {
+                        Thread.sleep(3000);
+                        return "我是子线程" + Thread.currentThread().getName();
+                    }
+                });
+        // 使用线程池运行
         executor.submit(futureTask);
+//        最后通过 FutureTask.get() 得到子线程的计算结果。
         String result = (String) futureTask.get();
         log.info("result is {}", result);
     }
 
+    //使用Thread运行FutureTask
     @Test
     public void testThreadByCallable() throws ExecutionException, InterruptedException {
         FutureTask futureTask = new FutureTask(new Callable<String>() {
